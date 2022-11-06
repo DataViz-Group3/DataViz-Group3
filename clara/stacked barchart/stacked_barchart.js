80aecf081089b9e95601a1bc97f5928f8c4e7141
claramouawad <clara1999m@gmail.com>
2022-11-06 11:17:57 +0100

    Worked on stacked barchart

--- clara/stacked barchart/stacked_barchart.js
+++ clara/stacked barchart/stacked_barchart.js
@@ -1,19 +1,23 @@
 function main() {
-    var svg = d3.select("svg"),
-    margin = 250,
-    width = svg.attr("width") - margin,
-    height = svg.attr("height") - margin;
+    // set the dimensions and margins of the graph
+    var margin = {top: 100, right: 0, bottom: 20, left: 50},
+        width = 1450 - margin.left - margin.right,
+        height = 650 - margin.top - margin.bottom;
+
+
+    var svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
     svg.append("text")
-    .attr("transform", "translate(100,0)")
     .attr("x", 0)
-    .attr("y", 50)
+    .attr("y", -40)
     .attr("font-size", "24px")
-    .text("Stacked Bar")
-
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + 100 + "," + 100 + ")");
+	.text("Stacked Barchart")
 
 			
     d3.csv("../../data_clean/a1_v2_stacked_chart.csv", function(data) {
@@ -29,19 +33,35 @@ function main() {
             .padding([0.2])
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).tickSizeOuter(0));
+            .call(d3.axisBottom(x).tickSizeOuter(0))
+            .selectAll("text")  
+            .attr("font-size", "9px")
+        svg.append("g")
+            .append("text")
+            .attr("x", 1400)
+            .attr("y", 550)
+            .attr('text-anchor', 'end')
+            .attr('stroke', 'black')
+            .text("Zone")
 
         // Add Y axis
         var y = d3.scaleLinear()
             .domain([0, 3050])
             .range([ height, 0 ]);
         svg.append("g")
-            .call(d3.axisLeft(y));
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 10)
+            .attr('dy', '-5em')
+            .attr('text-anchor', 'end')
+            .attr('stroke', 'black')
+            .text('Count');
 
         // color palette = one color per subgroup
         var color = d3.scaleOrdinal()
             .domain(subgroups)
-            .range(['red', 'blue', 'purple', 'yellow', 'grey', 'green'])
+            .range(["green", "yellow", "blue", "red", "purple", "grey"])
 
         //stack the data? --> stack per subgroup
         var stackedData = d3.stack()
@@ -63,5 +83,28 @@ function main() {
             .attr("y", function(d) { return y(d[1]); })
             .attr("height", function(d) { return y(d[0]) - y(d[1]); })
             .attr("width",x.bandwidth())
+
+        var legend = svg.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("text-anchor", "end")
+            .selectAll("g")
+            .data(subgroups.slice().reverse())
+            .enter().append("g")
+          //.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+           .attr("transform", function(d, i) { return "translate(-50," + (300 + i * 20) + ")"; });
+      
+        legend.append("rect")
+            .attr("x", width - 50)
+            .attr("y", -300)
+            .attr("width", 19)
+            .attr("height", 19)
+            .attr("fill", color);
+      
+        legend.append("text")
+            .attr("x", width - 55)
+            .attr("y", -287)
+            .text(function(d) { return d; });
+      
 })
 }
