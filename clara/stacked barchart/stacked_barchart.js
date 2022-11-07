4d4a0b8973a8b53dba89c2d095268b0ef9dc89eb
claramouawad <clara1999m@gmail.com>
2022-11-07 21:04:09 +0100

    made viz2 interactive

--- clara/stacked barchart/stacked_barchart.js
+++ clara/stacked barchart/stacked_barchart.js
@@ -1,7 +1,7 @@
 function main() {
     // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 0, bottom: 20, left: 50},
-        width = 1450 - margin.left - margin.right,
+    var margin = {top: 20, right: 0, bottom: 20, left: 200},
+        width = 1400 - margin.left - margin.right,
         height = 550 - margin.top - margin.bottom;
 
 
@@ -22,36 +22,35 @@ function main() {
         var groups = d3.map(data, function(d){return(d['Circoscrizione Name'])}).keys()
 
         // Add X axis
-        var x = d3.scaleBand()
+        var x = d3.scaleLinear()
+            .domain([0, 3050])
+            .range([0, width ]); 
+        
+        // Add Y axis        
+        var y = d3.scaleBand()
             .domain(groups)
-            .range([0, width])
+            .range([0, height])
             .padding([0.2])
+
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x).tickSizeOuter(0))
-            .selectAll("text")  
-            .attr("font-size", "9px")
-        svg.append("g")
             .append("text")
-            .attr("x", 1400)
-            .attr("y", 530)
             .attr('text-anchor', 'end')
+            .attr("x", width)
+            .attr("y", -5)
             .attr('stroke', 'black')
-            .text("Zone")
+            .text("Count")
 
-        // Add Y axis
-        var y = d3.scaleLinear()
-            .domain([0, 3050])
-            .range([ height, 0 ]);
+
         svg.append("g")
             .call(d3.axisLeft(y))
             .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 10)
-            .attr('dy', '-5em')
+            .attr('y', '-5')
+            .attr('x', '-5')
             .attr('text-anchor', 'end')
             .attr('stroke', 'black')
-            .text('Count');
+            .text('Zone');
 
         // color palette = one color per subgroup
 
@@ -64,6 +63,34 @@ function main() {
             .keys(subgroups)
             (data)
 
+        var tooltip = d3.select("#my_dataviz")
+        .append("div")
+        .style("position", "absolute")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("width", "auto")
+        .style("height", "auto")
+        // Three function that change the tooltip when user hover / move / leave a cell
+        var mouseover = function(d) {
+            var xPos = parseFloat(d3.select(this).attr('x'));
+            var yPos = parseFloat(d3.select(this).attr('y'));
+            var subgroupName = d3.select(this.parentNode).datum().key;
+            var subgroupValue = d.data[subgroupName];
+            tooltip
+                .html(subgroupName + ": " + subgroupValue)
+                .style("opacity", 1)
+                .style('left', xPos+margin.left +15+ 'px')
+                .style('top', yPos+153 + 'px')
+        }
+        
+        
+        var mouseleave = function(d) {
+            tooltip
+              .style("opacity", 0)
+        }
+                  
+        
+
         // Show the bars
         svg.append("g")
             .selectAll("g")
@@ -75,10 +102,16 @@ function main() {
             // enter a second time = loop subgroup per subgroup to add all rectangles
             .data(function(d) { return d; })
             .enter().append("rect")
-            .attr("x", function(d) { return x(d.data['Circoscrizione Name']); })
-            .attr("y", function(d) { return y(d[1]); })
-            .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-            .attr("width",x.bandwidth())
+            .attr("y", function(d) { return y(d.data['Circoscrizione Name']); })
+            .attr("x", function(d) { return x(d[0]); })
+            .attr("height", y.bandwidth())
+            .attr("width",function(d) { return x(d[1]) - x(d[0]); })
+            .on("mouseover", mouseover)
+            .on("mouseleave", mouseleave)
+
+            
+            
+
             
 
         var legend = svg.append("g")
@@ -93,14 +126,14 @@ function main() {
       
         legend.append("rect")
             .attr("x", width - 50)
-            .attr("y", -300)
+            .attr("y", -20)
             .attr("width", 19)
             .attr("height", 19)
             .attr("fill", color);
       
         legend.append("text")
             .attr("x", width - 55)
-            .attr("y", -287)
+            .attr("y", -7)
             .text(function(d) { return d; });
     		d3.select(this).attr('class','highlight')
 
