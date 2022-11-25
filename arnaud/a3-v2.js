45a982d404491e31861026b24df62e127ee8a605
Arnaud Ruymaekers <arnaudr85@gmail.com>
2022-11-25 22:03:21 +0100

    Map of quartiere too

--- arnaud/a3-v2.js
+++ arnaud/a3-v2.js
@@ -3,18 +3,44 @@ function drawChart_a3_v2() {
 
     //Width and height
     let width = 1000;
-    let height = 2000;
+    let height = 1100;
 
-    let normalize_name = function (name) {
-        return name.replaceAll(' ', '').replaceAll('.', '');
-    }
+    // Sizes definitions
+    const win_ratio = 3/2;
+    const win_width = d3.select(div_id).node().getBoundingClientRect().width;
+    const win_height = win_width / win_ratio;
 
-    let zones, mini_zones;
+    const margin = {top: 20, right: 20, bottom: 20, left: 20};
 
+    const main_win_width = (win_width*(2/3) ) - margin.right - margin.left;
+    const main_win_height = win_height - margin.top - margin.bottom;
+
+    const tool_win_width = (win_width/3) - margin.right - margin.left;
+
+    d3.select(div_id)
+        .style("display", "flex")
+        .style("flex-direction", "row")
+
+    // Basic definition of svg zone of main graph
     let svg = d3.select(div_id)
+        .append("div")
+        .style("border-style", "solid")
+        .style("width", main_win_width + "px")
+        .style("height", main_win_height + "px")
         .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("viewBox", "0 0 " + main_win_width + " " + main_win_height);
+
+    // Right toolbar
+    let toolBar = d3.select(div_id)
+        .append("div")
+        .style("width", tool_win_width + "px")
+        .style("height", main_win_height + "px");
+
+    // Mini map
+    let minimap = toolBar.append("div")
+        .style("border-style", "solid")
+        .style("width", tool_win_width + "px")
+        .style("height", tool_win_width + "px");
 
     // create a tooltip
     let Tooltip = d3.select(div_id)
@@ -30,85 +56,92 @@ function drawChart_a3_v2() {
         .style("pointer-events", "none")
         .style("padding", "5px");
 
-    // Load external data and boot
+    function getDataRatio(data) {
+        console.log(data)
+        function getGroupedVal (func, ax) {
+            return func(data, function (d){
+                return func(d.geometry.coordinates, function (d1) {
+                    return d1[0][ax];
+                })
+            })
+        }
+
+        const max_x = getGroupedVal(d3.max, 0);
+        const max_y = getGroupedVal(d3.max, 1);
+        const min_x = getGroupedVal(d3.min, 0);
+        const min_y = getGroupedVal(d3.min, 1);
+
+        return (max_x-min_x) / (max_y-min_y);
+    }
+
+    function normalize_name(name) {
+        return name.replaceAll(' ', '').replaceAll('.', '');
+    }
+
+    d3.json("../data_clean/circo_poli.json").then( function(data) {
+        let quart_data = data["Quartiere Name"];
+        quart_data = Object.keys(quart_data).map((key) => [key, quart_data[key]]);
+
+        let quart_divs = minimap
+            .selectAll("quart_divs")
+            .data(quart_data)
+            .enter()
+            .append("div")
+            .style("position", "absolute")
+            .attr("id", function(d) { return d[0]; });
+
+        d3.json("../data/poli_sociali.json").then( function(data) {
+            function poli_in_circo(circo) {
+                return data.features.filter(function(poli) {
+                    return circo.includes(poli.properties.nome_quart)
+                });
+            }
+
+            quart_divs.append("svg")
+                .attr("width", tool_win_width)
+                .attr("height", tool_win_width)
+                .append("g")
+                .selectAll("quart")
+                .data(function(circo) {
+                    return poli_in_circo(circo[1]);
+                })
+                .join("path")
+                .attr("d", d3.geoPath()
+                        .projection(
+                            d3.geoIdentity()
+                                .reflectY(true)
+                                .fitSize([tool_win_width, tool_win_width], data)
+                        )
+
+                )
+                .style("fill", "white")
+                .style("stroke", "black");
+        })
+    })
+
+    let zones;
     d3.json("../data/circoscrizioni.json").then( function(data) {
-
-        function getGroupedVal (func, ax) {
-            return func(data.features, function (d){
-                return func(d.geometry.coordinates, function (d1) {
-                    return d1[0][ax];
-                })
-            })
-        }
-
-        const max_x = getGroupedVal(d3.max, 0);
-        const max_y = getGroupedVal(d3.max, 1);
-        const min_x = getGroupedVal(d3.min, 0);
-        const min_y = getGroupedVal(d3.min, 1);
-
-        const ratio = (max_x-min_x) / (max_y-min_y);
-
-        // Map and projection
-        const projection = d3.geoIdentity()
-            .reflectY(true)
-            .fitSize([width, width*ratio], data);
+        const ratio = getDataRatio(data.features);
 
         // Draw the map
         zones = svg
             .append("g")
+            //.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
             .selectAll(".zones")
             .data(data.features)
             .join("path")
             .attr("class", "zones")
             .attr("id", function (d){ return normalize_name("zone_" + d.properties.nome); } )
             .attr("d", d3.geoPath()
-                .projection(projection)
+                .projection(
+                    d3.geoIdentity()
+                        .reflectY(true)
+                        .fitSize([main_win_width, main_win_width], data)
+                )
             )
             .attr("fill", "white")
-            .style("stroke", "black")
-
+            .style("stroke", "black");
     })
-    /*
-    d3.json("../data/poli_sociali.json").then( function(data) {
-
-        console.log(d3.filter(data.features, function (d) { console.log(d)}))
-
-        function getGroupedVal (func, ax) {
-            return func(data.features, function (d){
-                return func(d.geometry.coordinates, function (d1) {
-                    return d1[0][ax];
-                })
-            })
-        }
-
-        const max_x = getGroupedVal(d3.max, 0);
-        const max_y = getGroupedVal(d3.max, 1);
-        const min_x = getGroupedVal(d3.min, 0);
-        const min_y = getGroupedVal(d3.min, 1);
-
-        const ratio = (max_x-min_x) / (max_y-min_y);
-
-        // Map and projection
-        const projection = d3.geoIdentity()
-            .reflectY(true)
-            .fitSize([width, width*ratio], data);
-
-        // Draw the map
-        zones = svg
-            .append("g")
-            .selectAll(".zones")
-            .data(data.features)
-            .join("path")
-            .attr("class", "zones")
-            .attr("id", function (d){ return normalize_name("zone_" + d.properties.nome); } )
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .attr("fill", "white")
-            .style("stroke", "red")
-    })
-
-     */
 
     d3.csv("../data_clean/trees_located.csv").then( function(data) {
         const subgroup_data = d3.groups(data, d => d["Circoscrizione Name"]);
