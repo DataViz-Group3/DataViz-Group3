0132b834f4efd4fa45d54911cda73d749c6ea9ed
Arnaud Ruymaekers <arnaudr85@gmail.com>
2022-11-28 19:40:38 +0100

    Use new dataset

--- arnaud/a3-v2.js
+++ arnaud/a3-v2.js
@@ -56,129 +56,58 @@ function drawChart_a3_v2() {
         .style("pointer-events", "none")
         .style("padding", "5px");
 
-    function getDataRatio(data) {
-        console.log(data)
-        function getGroupedVal (func, ax) {
-            return func(data, function (d){
-                return func(d.geometry.coordinates, function (d1) {
-                    return d1[0][ax];
-                })
-            })
-        }
-
-        const max_x = getGroupedVal(d3.max, 0);
-        const max_y = getGroupedVal(d3.max, 1);
-        const min_x = getGroupedVal(d3.min, 0);
-        const min_y = getGroupedVal(d3.min, 1);
-
-        return (max_x-min_x) / (max_y-min_y);
-    }
-
     function normalize_name(name) {
         return name.replaceAll(' ', '').replaceAll('.', '');
     }
 
-    d3.json("../data_clean/circo_poli.json").then( function(data) {
-        let quart_data = data["Quartiere Name"];
-        quart_data = Object.keys(quart_data).map((key) => [key, quart_data[key]]);
+    let minimap_divs = {};
 
-        let quart_divs = minimap
-            .selectAll("quart_divs")
-            .data(quart_data)
-            .enter()
-            .append("div")
-            .style("position", "absolute")
-            .attr("id", function(d) { return d[0]; });
+    d3.json("../data_clean/a3.json").then( function(data) {
+        const measure = "Canopy Cover Density (perc)";
 
-        d3.json("../data/poli_sociali.json").then( function(data) {
-            function poli_in_circo(circo) {
-                return data.features.filter(function(poli) {
-                    return circo.includes(poli.properties.nome_quart)
-                });
-            }
-
-            quart_divs.append("svg")
-                .attr("width", tool_win_width)
-                .attr("height", tool_win_width)
-                .append("g")
-                .selectAll("quart")
-                .data(function(circo) {
-                    return poli_in_circo(circo[1]);
-                })
-                .join("path")
-                .attr("d", d3.geoPath()
-                        .projection(
-                            d3.geoIdentity()
-                                .reflectY(true)
-                                .fitSize([tool_win_width, tool_win_width], data)
-                        )
-
-                )
-                .style("fill", "white")
-                .style("stroke", "black");
-        })
-    })
-
-    let zones;
-    d3.json("../data/circoscrizioni.json").then( function(data) {
-        const ratio = getDataRatio(data.features);
-
-        // Draw the map
-        zones = svg
-            .append("g")
-            //.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .selectAll(".zones")
-            .data(data.features)
-            .join("path")
-            .attr("class", "zones")
-            .attr("id", function (d){ return normalize_name("zone_" + d.properties.nome); } )
-            .attr("d", d3.geoPath()
-                .projection(
-                    d3.geoIdentity()
-                        .reflectY(true)
-                        .fitSize([main_win_width, main_win_width], data)
-                )
-            )
-            .attr("fill", "white")
-            .style("stroke", "black");
-    })
-
-    d3.csv("../data_clean/trees_located.csv").then( function(data) {
-        const subgroup_data = d3.groups(data, d => d["Circoscrizione Name"]);
-
-        let subgroup_data_dict = {};
-        for (let i in subgroup_data) {
-            subgroup_data_dict[subgroup_data[i][0]] = subgroup_data[i][1];
-        }
-
-        let subgroup_densities = {};
-        let max_dens = 0;
-        for (let subgroup in subgroup_data_dict) {
-            const subgroup_area = subgroup_data_dict[subgroup][0]["Circoscrizione Area"];
-            subgroup_densities[subgroup] = (100 * d3.sum(subgroup_data_dict[subgroup], function (d){
-                return d["Canopy Cover (m2)"];
-            }) / subgroup_area).toFixed(2);
-            if (max_dens < subgroup_densities[subgroup]) {
-                max_dens = subgroup_densities[subgroup];
-            }
-        }
-
+        /* ---------------------------------------------------------------------------------------------
+        Color Range
+        --------------------------------------------------------------------------------------------- */
+        const max_dens = d3.max(data.features, (d) => d.properties[measure])
         const colors = d3.scaleLinear()
             .domain([0, max_dens])
             .range(["white", "darkgreen"])
 
-        // Three function that change the tooltip when user hover / move / leave a cell
-        let mouseover = function (event, d) {
-            const circ = d.properties.nome;
-            d3.selectAll("#zone_" + normalize_name(circ))
+        /* ---------------------------------------------------------------------------------------------
+        Main svg
+        --------------------------------------------------------------------------------------------- */
+        const projection = d3.geoIdentity()
+            .reflectY(true)
+            .fitSize([main_win_width, main_win_width], data);
+
+        let zones = svg.append("g")
+            .selectAll(".zones")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("class", "zones")
+            .attr("id", (d) => normalize_name(d.properties.Name) )
+            .attr("d", d3.geoPath().projection(projection))
+            .attr("fill", (d) => colors(d.properties[measure]))
+            .style("stroke", "black");
+
+        /* ---------------------------------------------------------------------------------------------
+        Hover functions
+        --------------------------------------------------------------------------------------------- */
+        function mouseover(event, d) {
+            const circ = d.properties.Name;
+            zones.style("opacity", 0.8);
+            d3.selectAll("#" + normalize_name(circ) + ".zones")
                 .style("stroke-width", 3)
                 .style("opacity", 1);
             Tooltip.style("opacity", 1);
         }
-        let mousemove = function (event, d) {
-            const circ = d.properties.nome;
-            const density = subgroup_densities[circ];
-            const tot_area = subgroup_data_dict[circ][0]["Circoscrizione Area"];
+        zones.on("mouseover", mouseover);
+
+        function mousemove(event, d) {
+            const circ = d.properties.Name;
+            const density = d.properties["Canopy Cover Density (perc)"];
+            const tot_area = d.properties["Area"];
             Tooltip.html(
                 circ + "<br>" +
                 "Total area: " + tot_area + " m2<br>" +
@@ -187,23 +116,66 @@ function drawChart_a3_v2() {
                 .style("left", (event.pageX+20) + "px")
                 .style("top", (event.pageY) + "px");
         }
-        let mouseleave = function (event, d) {
-            const circ = d.properties.nome;
-            d3.selectAll("#zone_" + normalize_name(circ))
+        zones.on("mousemove", mousemove)
+
+        function mouseleave(event, d) {
+            const circ = d.properties.Name;
+            zones.style("opacity", 1);
+            d3.selectAll("#" + normalize_name(circ) + ".zones")
                 .style("stroke-width", 1)
                 .style("opacity", 0.8);
             Tooltip.style("opacity", 0);
         }
+        zones.on("mouseleave", mouseleave)
 
-        zones.attr("fill", function (d){
-                const circ = d.properties.nome;
-                return colors(subgroup_densities[circ]);
-            })
-            .style("opacity", 0.8)
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
-    })
+        /* ---------------------------------------------------------------------------------------------
+        Minimap zones
+        --------------------------------------------------------------------------------------------- */
+        const circos = data.features.map((d) => [d.properties.Name, d.properties['Quartiere']]);
+        for (let i in circos){
+            const circo_name = circos[i][0];
+            const circo_data = circos[i][1];
+
+            let minimap_svg = minimap.append("div")
+                .style("opacity", 0)
+                .style("position", "absolute")
+                .attr("id", "minimap_" + normalize_name(circo_name))
+                .append("svg")
+                .attr("width", tool_win_width)
+                .attr("height", tool_win_width);
+
+            minimap_svg.append("g")
+                .selectAll("quart")
+                .data(circo_data.features)
+                .enter()
+                .append("path")
+                .attr("d", d3.geoPath().projection(
+                    d3.geoIdentity()
+                        .reflectY(true)
+                        .fitSize([tool_win_width, tool_win_width], circo_data)
+                ))
+                .style("fill", (d) => colors(d.properties[measure]))
+                .style("stroke", "black");
+        }
+
+        /* ---------------------------------------------------------------------------------------------
+        Minimap toggle
+        --------------------------------------------------------------------------------------------- */
+        let active_minimap = null;
+        function mouseclick(event, d) {
+            const circo = normalize_name(d.properties.Name);
+            if (active_minimap === circo) {
+                d3.select("#minimap_" + active_minimap).style("opacity", 0);
+                active_minimap = null;
+            }
+            else {
+                d3.select("#minimap_" + active_minimap).style("opacity", 0);
+                d3.select("#minimap_" + circo).style("opacity", 1);
+                active_minimap = circo;
+            }
+        }
+        zones.on("click", mouseclick);
+    });
 }
 
 drawChart_a3_v2();
