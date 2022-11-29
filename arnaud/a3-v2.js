2ab402d8916f70b6bb3450df1a7f553f2084218e
Arnaud Ruymaekers <arnaudr85@gmail.com>
2022-11-29 02:12:28 +0100

    Finished viz 2

--- arnaud/a3-v2.js
+++ arnaud/a3-v2.js
@@ -68,10 +68,23 @@ function drawChart_a3_v2() {
         /* ---------------------------------------------------------------------------------------------
         Color Range
         --------------------------------------------------------------------------------------------- */
-        const max_dens = d3.max(data.features, (d) => d.properties[measure])
-        const colors = d3.scaleLinear()
-            .domain([0, max_dens])
-            .range(["white", "darkgreen"])
+        const max_dens = d3.max(data.features, (d) => d.properties[measure]);
+        const max_dens_quart = d3.max(data.features,
+            (d) => d3.max(d.properties['Quartiere'].features,
+                (d) => d.properties[measure]));
+        const max_measure = max_dens > max_dens_quart? max_dens: max_dens_quart;
+
+        const colors = d3.scaleThreshold()
+            .domain([
+                0.05,
+                0.1,
+                0.2,
+                0.5,
+                1.5,
+                3,
+                max_measure
+            ])
+            .range(d3.schemeGreens[7])
 
         /* ---------------------------------------------------------------------------------------------
         Main svg
@@ -106,12 +119,14 @@ function drawChart_a3_v2() {
 
         function mousemove(event, d) {
             const circ = d.properties.Name;
-            const density = d.properties["Canopy Cover Density (perc)"];
-            const tot_area = d.properties["Area"];
+            const cover = Math.round(d.properties["Canopy Cover (m2)"] * 100) / 100;
+            const density = Math.round(d.properties["Canopy Cover Density (perc)"] * 100) / 100;
+            const tot_area = Math.round(d.properties["Area"] / 10000) / 100;
             Tooltip.html(
                 circ + "<br>" +
-                "Total area: " + tot_area + " m2<br>" +
-                density + "% of area covered in trees"
+                "Total area: " + tot_area + " km2<br>" +
+                density + "% of area covered in trees<br>" +
+                cover + " m2 of canopy cover"
             )
                 .style("left", (event.pageX+20) + "px")
                 .style("top", (event.pageY) + "px");
@@ -131,31 +146,100 @@ function drawChart_a3_v2() {
         /* ---------------------------------------------------------------------------------------------
         Minimap zones
         --------------------------------------------------------------------------------------------- */
-        const circos = data.features.map((d) => [d.properties.Name, d.properties['Quartiere']]);
+        const circos = data.features.map((d) => [d.properties.Name, d]);
         for (let i in circos){
             const circo_name = circos[i][0];
-            const circo_data = circos[i][1];
+            const circo_data = circos[i][1].properties['Quartiere'];
+
+            const mini_projection = d3.geoIdentity()
+                .reflectY(true)
+                .fitSize([tool_win_width, tool_win_width], circo_data);
 
             let minimap_svg = minimap.append("div")
                 .style("opacity", 0)
+                .style("pointer-events", "none")
                 .style("position", "absolute")
-                .attr("id", "minimap_" + normalize_name(circo_name))
+                .attr("class", "minimap")
+                .attr("id", normalize_name(circo_name))
                 .append("svg")
                 .attr("width", tool_win_width)
                 .attr("height", tool_win_width);
 
-            minimap_svg.append("g")
+            let minimap_zones = minimap_svg.append("g")
                 .selectAll("quart")
                 .data(circo_data.features)
                 .enter()
                 .append("path")
-                .attr("d", d3.geoPath().projection(
-                    d3.geoIdentity()
-                        .reflectY(true)
-                        .fitSize([tool_win_width, tool_win_width], circo_data)
-                ))
+                .attr("class", "mini_zones")
+                .attr("id", (d) => normalize_name(d.properties.Name) )
+                .attr("d", d3.geoPath().projection(mini_projection))
                 .style("fill", (d) => colors(d.properties[measure]))
                 .style("stroke", "black");
+
+            /* ---------------------------------------------------------------------------------------------
+            Minimap Border
+            --------------------------------------------------------------------------------------------- */
+            const circo_features = data.features;
+            const circo_border = circo_features.filter((d) => d.properties.Name === circo_name);
+            const filtered_data = {
+                "features": circo_border,
+                "type": "FeatureCollection"
+            };
+            const border_projection = d3.geoIdentity()
+                .reflectY(true)
+                .fitSize([tool_win_width, tool_win_width], filtered_data);
+
+            minimap_svg.append("g")
+                .selectAll("border")
+                .data(circo_border)
+                .enter()
+                .append("path")
+                .attr("d", d3.geoPath().projection(border_projection))
+                .style("pointer-events", "none")
+                .style("fill", "none")
+                .style("stroke", "black")
+                .style("stroke-width",3);
+
+            /* ---------------------------------------------------------------------------------------------
+            Minimap Hover functions
+            --------------------------------------------------------------------------------------------- */
+            function mouseover_mini(event, d) {
+                const quart = d.properties.Name;
+                d3.selectAll(".mini_zones").style("opacity", 0.8);
+                d3.selectAll("#" + normalize_name(quart) + ".mini_zones")
+                    .style("stroke-width", 3)
+                    .style("opacity", 1);
+                Tooltip.style("opacity", 1);
+            }
+            minimap_zones.on("mouseover", mouseover_mini);
+
+            function mousemove_mini(event, d) {
+                const quart = d.properties.Name;
+                const cover = Math.round(d.properties["Canopy Cover (m2)"] * 100) / 100;
+                const density = Math.round(d.properties["Canopy Cover Density (perc)"] * 100) / 100;
+                const tot_area = Math.round(d.properties["Area"] / 10000) / 100;
+                Tooltip.html(
+                    quart + "<br>" +
+                    "Total area: " + tot_area + " km2<br>" +
+                    density + "% of area covered in trees<br>" +
+                    cover + " m2 of canopy cover"
+                )
+                    .style("left", (event.pageX+20) + "px")
+                    .style("top", (event.pageY) + "px");
+            }
+            minimap_zones.on("mousemove", mousemove_mini);
+
+            function mouseleave_mini(event, d) {
+                const quart = d.properties.Name;
+                d3.selectAll(".mini_zones").style("opacity", 1);
+                d3.selectAll("#" + normalize_name(quart) + ".mini_zones")
+                    .style("stroke-width", 1)
+                    .style("opacity", 0.8);
+                Tooltip.style("opacity", 0);
+            }
+            minimap_zones.on("mouseleave", mouseleave_mini);
+
+
         }
 
         /* ---------------------------------------------------------------------------------------------
@@ -165,16 +249,66 @@ function drawChart_a3_v2() {
         function mouseclick(event, d) {
             const circo = normalize_name(d.properties.Name);
             if (active_minimap === circo) {
-                d3.select("#minimap_" + active_minimap).style("opacity", 0);
+                d3.select("#" + active_minimap + ".minimap")
+                    .style("opacity", 0)
+                    .style("pointer-events", "none");
                 active_minimap = null;
             }
             else {
-                d3.select("#minimap_" + active_minimap).style("opacity", 0);
-                d3.select("#minimap_" + circo).style("opacity", 1);
+                d3.select("#" + active_minimap + ".minimap")
+                    .style("opacity", 0)
+                    .style("pointer-events", "none");
+                d3.select("#" + circo + ".minimap")
+                    .style("opacity", 1)
+                    .style("pointer-events", "auto");
                 active_minimap = circo;
             }
         }
         zones.on("click", mouseclick);
+
+        /* ---------------------------------------------------------------------------------------------
+        Legend
+        --------------------------------------------------------------------------------------------- */
+        /*var legend = svg.selectAll('g.legendEntry')
+            .data(colors.range())
+            .enter()
+            .append('g').attr('class', 'legendEntry');
+
+        legend
+            .append('rect')
+            .attr("x", 120)
+            .attr("y", function(d, i) {
+                return 30+ i * 30;
+            })
+            .attr("width", 20)
+            .attr("height", 20)
+            .style("stroke", "black")
+            .style("stroke-width", 1)
+            .style("fill", function(d){return d;});
+        //the data objects are the fill colors
+
+        legend
+            .append('text')
+            .attr("x", 150) //leave 5 pixel space after the <rect>
+            .attr("y", function(d, i) {
+                return 30+ i * 30;
+            })
+            .attr("dy", "0.8em") //place text one line *below* the x,y point
+            .text(function(d,i) {
+                var extent = colors.invertExtent(d);
+                //extent will be a two-element array, format it however you want:
+                var format = d3.format("0.2f");
+                if( i == 0 ) return "< " + format(+extent[1]);
+                if(i == 6) return ">" + format(+extent[0]);
+                return format(+extent[0]) + " - " + format(+extent[1]);
+            });
+        legend.append('text')
+            .attr("x", 120)
+            .attr("y", 20)
+            .text("Tree Abundance")
+
+         */
+
     });
 }
 
